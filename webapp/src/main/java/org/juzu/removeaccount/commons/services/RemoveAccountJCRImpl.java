fcc22563fe26c7d9db53e85f8aca56dbe22cebfc
anhvt <anhvta@yahoo.com>
2015-01-15 08:58:45 +0700

    ORG-1315: add webui filter and portlet filter

--- webapp/src/main/java/org/juzu/removeaccount/commons/services/RemoveAccountJCRImpl.java
+++ webapp/src/main/java/org/juzu/removeaccount/commons/services/RemoveAccountJCRImpl.java
@@ -10,7 +10,10 @@ import org.juzu.removeaccount.commons.models.Account;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.jcr.Node;
+import javax.jcr.*;
+import javax.jcr.lock.LockException;
+import javax.jcr.nodetype.ConstraintViolationException;
+import javax.jcr.version.VersionException;
 
 /**
  * Created by anhvt on 08/01/15.
@@ -21,8 +24,6 @@ public class RemoveAccountJCRImpl implements RemoveAccountService {
   private static final Log log             = ExoLogger.getLogger(RemoveAccountJCRImpl.class);
 
   private static final String REMOVE_ACCOUNT_APP = "RemoveAccountApplication";
-
-  private static final String CREATED_DATE = "exo:createdDate";
 
   private static final String  NODE_PROPS_USERNAME = "exo:username";
 
@@ -57,9 +58,59 @@ public class RemoveAccountJCRImpl implements RemoveAccountService {
     }
     return removeAccApp;
   }
+  private void setNodeProperties(Node aNode,Account account) throws RepositoryException {
+    aNode.setProperty(NODE_PROPS_USERNAME,account.getUsername());
+    aNode.setProperty(NODE_PROPS_REASON,account.getReason());
+    aNode.setProperty(NODE_PROPS_SOC_NETWORK,account.getSocialNetwork());
+    aNode.setProperty(NODE_PROPS_UNSUBSCRIBE_MKT_EMAIL,account.getUnsubscibeMarketingEmail());
+  }
+  private Account transferNode2Account(Node node) throws RepositoryException {
+    if (null == node)
+      return null;
+    Account account = new Account("");
+    PropertyIterator iter = node.getProperties("exo:*");
+    Property p;
+    String name;
+    while (iter.hasNext()) {
+      p = iter.nextProperty();
+      name = p.getName();
+      if (name.equals(NODE_PROPS_USERNAME)){
+        account.setUsername(p.getString());
+      }else if (name.equals(NODE_PROPS_REASON)){
+        account.setReason(p.getString());
+      }else if (name.equals(NODE_PROPS_SOC_NETWORK)){
+        account.setSocialNetwork(p.getString());
+      }else if (name.equals(NODE_PROPS_UNSUBSCRIBE_MKT_EMAIL)){
+        account.setUnsubscibeMarketingEmail(p.getBoolean());
+      }
+    }
+    if (account.checkValid())
+      return account;
+
+    return null;
+  }
 
   @Override
   public Account storeAccount(Account account) {
+    Node homeApp = null;
+    try {
+      homeApp = this.getOrCreateRemoveAccountAppHome();
+      Node accountNode = null;
+      if (null != homeApp){
+        if (homeApp.hasNode(account.getUsername())){
+          accountNode = homeApp.getNode(account.getUsername());
+        }else{
+          accountNode = homeApp.addNode(account.getUsername());
+          this.setNodeProperties(accountNode,account);
+          homeApp.save();
+        }
+
+      }
+      return this.transferNode2Account(accountNode);
+    } catch (Exception e) {
+      log.error("ERR remove account cannot store remove account info ");
+      e.getMessage();
+    }
     return null;
   }
 
--- webapp/src/main/java/org/juzu/removeaccount/portlet/frontend/assets/javascripts/removeaccount.js
+++ webapp/src/main/java/org/juzu/removeaccount/portlet/frontend/assets/javascripts/removeaccount.js
@@ -1,3 +1,8 @@
 /**
  * Created by exoplatform on 08/01/15.
  */
+(function($){
+  $(document).ready(function () {
+    console.info("remove account js");
+  });
+})($);
