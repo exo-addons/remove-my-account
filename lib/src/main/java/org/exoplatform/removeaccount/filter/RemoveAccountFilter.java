65d8e909bec0e7af4544f38277a558faa6e28c6d
anhvt <anhvta@yahoo.com>
2015-01-15 14:26:43 +0700

    ORG-1315:apply logic to hide user non existing

--- lib/src/main/java/org/exoplatform/removeaccount/filter/RemoveAccountFilter.java
+++ lib/src/main/java/org/exoplatform/removeaccount/filter/RemoveAccountFilter.java
@@ -1,7 +1,10 @@
 package org.exoplatform.removeaccount.filter;
 
+import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.UserHandler;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -18,11 +21,28 @@ public class RemoveAccountFilter implements org.exoplatform.web.filter.Filter  {
   private final static Log log = ExoLogger.getLogger(RemoveAccountFilter.class);
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    log.info(" i am remove account filter from webui ");
+    // check if you is already login
     HttpServletRequest req = (HttpServletRequest)request;
     HttpServletResponse res = (HttpServletResponse)response;
-    res.sendRedirect("/portal/intranet/profile");
-    return;
-//    chain.doFilter(request,response);
+    String currentUser = req.getRemoteUser();
+    if ( null != currentUser) {
+      try{
+        String reqURI = req.getRequestURI();
+        String[] paths = reqURI.split("/");
+        String usernameVisivited = paths[paths.length-1];
+        if(!currentUser.equals(usernameVisivited)){
+          UserHandler userHandler = CommonsUtils.getService(OrganizationService.class).getUserHandler();
+          if (null == userHandler.findUserByName(usernameVisivited)){
+            log.info(" webui remove filter cannot find user for "+usernameVisivited);
+            res.sendRedirect("/portal/intranet/profile");
+            return;
+          }
+        }
+      }catch (Exception e){
+        log.error("ERR remove account webui filter "+e.getMessage());
+      }
+    }
+    chain.doFilter(request,response);
   }
+
 }
--- webapp/src/main/java/org/juzu/removeaccount/commons/filters/RemoveAccountPortletFilter.java
+++ webapp/src/main/java/org/juzu/removeaccount/commons/filters/RemoveAccountPortletFilter.java
@@ -4,19 +4,16 @@ import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 
 import javax.portlet.*;
-import javax.portlet.filter.ActionFilter;
 import javax.portlet.filter.FilterChain;
 import javax.portlet.filter.FilterConfig;
 import javax.portlet.filter.RenderFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * Created by exoplatform on 14/01/15.
  */
-public class RemoveAccountFilter implements RenderFilter {
-  private final static Log log = ExoLogger.getLogger(RemoveAccountFilter.class);
+public class RemoveAccountPortletFilter implements RenderFilter {
+  private final static Log log = ExoLogger.getLogger(RemoveAccountPortletFilter.class);
 
   @Override
   public void doFilter(RenderRequest request, RenderResponse response, FilterChain chain) throws IOException, PortletException {
