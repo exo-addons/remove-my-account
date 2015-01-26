7086c388c0c547c3b2d28ffb037d680a00fe7751
anhvt <anhvta@yahoo.com>
2015-01-26 16:30:20 +0700

    ORG-1334:remvove also cookie rememberme

--- lib/src/main/java/org/exoplatform/removemyaccount/filter/RemoveMyAccountSuccessFilter.java
+++ lib/src/main/java/org/exoplatform/removemyaccount/filter/RemoveMyAccountSuccessFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,11 +18,21 @@ import java.io.IOException;
  */
 public class RemoveMyAccountSuccessFilter implements org.exoplatform.web.filter.Filter  {
   private final static Log log = ExoLogger.getLogger(RemoveMyAccountSuccessFilter.class);
+  public static final String COOKIE_NAME_OAUTH = "rememberoauth";
+  public static final String COOKIE_NAME_EXO = "rememberme";
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
     HttpServletRequest req = (HttpServletRequest) servletRequest;
     HttpServletResponse res = (HttpServletResponse) servletResponse;
     HttpSession session = req.getSession();
+    Cookie cookie = new Cookie(COOKIE_NAME_OAUTH, "");
+    cookie.setPath(req.getContextPath());
+    cookie.setMaxAge(0);
+    res.addCookie(cookie);
+    cookie = new Cookie(COOKIE_NAME_EXO, "");
+    cookie.setPath(req.getContextPath());
+    cookie.setMaxAge(0);
+    res.addCookie(cookie);
     session.invalidate();
     res.sendRedirect("/portal");
     return;
